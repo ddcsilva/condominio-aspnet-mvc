88d0c68ab657635ccc972eeec8503c6e6b4ba23c
danilo.silva <danilo.silva@msn.com>
2023-04-16 18:39:02 -0300

    Criação dos Projetos Iniciais e classe Contexto

---
+++ GerenciadorCondominios/GerenciadorCondominios.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GerenciadorCondominios.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorCondominios.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+    }
+}
