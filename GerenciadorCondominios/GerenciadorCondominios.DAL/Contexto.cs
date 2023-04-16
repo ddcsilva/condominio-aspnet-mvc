f2861e6b5602b06ca16219c8454a7b4f2743f137
danilo.silva <danilo.silva@msn.com>
2023-04-16 19:32:09 -0300

    Configurando o Contexto

--- GerenciadorCondominios/GerenciadorCondominios.DAL/Contexto.cs
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Contexto.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GerenciadorCondominios.BLL.Models;
+using GerenciadorCondominios.DAL.Mapeamentos;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace GerenciadorCondominios.DAL;
 
-public class Contexto : DbContext
+public class Contexto : IdentityDbContext<Usuario, Funcao, string>
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options)
     {
+
+    }
+
+    public DbSet<Aluguel> Alugueis { get; set; }
+    public DbSet<Apartamento> Apartamentos { get; set; }
+    public DbSet<Evento> Eventos { get; set; }
+    public DbSet<Funcao> Funcoes { get; set; }
+    public DbSet<HistoricoRecursos> HistoricoRecursos { get; set; }
+    public DbSet<Mes> Meses { get; set; }
+    public DbSet<Pagamento> Pagamentos { get; set; }
+    public DbSet<Servico> Servicos { get; set; }
+    public DbSet<ServicoPredio> ServicoPredios { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Veiculo> Veiculos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(Contexto).Assembly);
     }
 }
